ab60b54c6f2f5ddb28b564937148fa0d425949c4
Mokbul Hussain <mokbulhussain2@gmail.com>
2022-12-06 08:50:12 +0600

    name route

--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-Route::get('/about',function (){
+Route::get('/about-us',function (){
     return view('about');
-});
+})->name('about');
 
-Route::get('/contact',function(){
+Route::get('/contact-page',function(){
     return view('contact');
-});
+})->name('contact');
 
-Route::get('/service',function (){
+Route::get('/service-page',function (){
     return view('service');
-});
+})->name('service');
